19279add9ff4260aa34d25961361f3f051be7ae9
D36u99er <chunwen86@gmail.com>
2025-09-10 22:44:32 +0800

    Add .def file for proper DLL exports

--- compile_direct.bat
+++ compile_direct.bat
@@ -2,7 +2,7 @@
 echo Compiling WinosTestPlugin.dll directly with cl.exe...
 
 REM Compile with MSVC compiler directly
-cl.exe /LD /Fe:WinosTestPlugin.dll WinosTestPlugin.cpp user32.lib
+cl.exe /LD /Fe:WinosTestPlugin.dll WinosTestPlugin.cpp /DEF:WinosTestPlugin.def user32.lib
 
 echo Compilation complete! WinosTestPlugin.dll should be generated.
 
